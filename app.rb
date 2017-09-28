6dc4d804577002ac35fbe2849ba1c5bae403d171
Erik Zakrzewski and Rafael Furry <erik@>
2017-09-28 11:13:55 -0700

    add working funtionality to all methods

--- app.rb
+++ app.rb
@@ -15,19 +15,39 @@ end
 
 get '/categories' do
   @categories = Category.all
+  @categories.each do |category|
+    if category.recipes == []
+      category.destroy
+    end
+  end
   erb :categories
+end
+
+get '/ingredients' do
+  @ingredients = Ingredient.all
+  @ingredients.each do |ingredient|
+    if ingredient.recipes == []
+      ingredient.destroy
+    end
+  end
+  erb :ingredients
 end
 
 post '/recipes' do
   name = params.fetch 'name'
   instructions = params.fetch 'instructions'
   rating = params.fetch 'rating'
-  category = params.fetch 'category'
-  cat = Category.find_or_initialize_by group: category
-  cat.save
+
   recipe = Recipe.create({:name => name, :instruction => instructions, :rating => rating})
 
-  recipe.categories.push(cat)
+  categories = params.fetch('category').split('#')
+  categories.each do |category|
+    if category.length > 0
+      cat = Category.find_or_initialize_by group: category
+      cat.save
+      recipe.categories.push(cat)
+    end
+  end
 
   ingredients = params.fetch('ingredients').split(', ')
   ingredients.each do |ingredient|
@@ -38,23 +58,29 @@ post '/recipes' do
   redirect '/recipes'
 end
 
-post '/categories' do
-  group = params.fetch 'group'
-  Category.create({:group => group})
-  redirect '/categories'
-end
-
 get '/recipes/:id' do
   @recipe = Recipe.find(params.fetch('id').to_i)
-  # binding.pry
   @categories = @recipe.categories
 
   erb :recipe
 end
 
 get '/categories/:id' do
-  # @category = Category.find(params.fetch('id').to_i)
-  # @recipes = Recipe.find(params.fetch('id').to_i)
+  @category = Category.find(params.fetch('id').to_i)
+  @recipes = @category.recipes
+  erb :category
+end
+
+get '/ingredients/:id' do
+  @ingredient = Ingredient.find(params.fetch('id').to_i)
+  @recipes = @ingredient.recipes
+  erb :ingredient
+end
+
+delete '/recipes/:id' do
+  recipe = Recipe.find(params.fetch(:id).to_i)
+  recipe.destroy
+  redirect '/recipes'
 end
 
 # patch '/recipes/:id' do
